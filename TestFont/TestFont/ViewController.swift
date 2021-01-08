76b7ca1c86e896d4b8d63b85c8aba314b0ecd93e
Roy Sparrow <rvxl2013@gmail.com>
2021-01-08 16:31:53 +0800

    add: test custom font flow

--- TestFont/TestFont/ViewController.swift
+++ TestFont/TestFont/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var systemFontLabel: UILabel!
+    
+    @IBOutlet weak var customFontLabel: UILabel!
+    
+    private let testStr = "8888"
+    
+    private let fontSize: CGFloat = 12
+    
+    private let letterSpacing: CGFloat = -0.2
+    
+    private lazy var systemFont = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+    
+    private lazy var customFont = UIFont(name: "Avenir", size: 12)
+//    private lazy var customFont = UIFont(name: "LiGothicMed", size: 12)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setup()
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        printLabelWidth()
+    }
+    
+    private func setup() {
+        
+        var fontFamilyNames = UIFont.familyNames
+        fontFamilyNames.sort()
+        fontFamilyNames.forEach({
+            print("font family: \($0)")
+        })
+        
+        systemFontLabel.font = systemFont
+        if let customFont = customFont {
+            customFontLabel.font = customFont
+        }
+        
+        let attrStr = NSMutableAttributedString(string: testStr)
+        attrStr.addAttribute(.kern, value: letterSpacing, range: NSMakeRange(0, attrStr.length))
+        
+        systemFontLabel.attributedText = attrStr
+        systemFontLabel.textAlignment = .left
+        customFontLabel.attributedText = attrStr
+        customFontLabel.textAlignment = .left
+    }
+    
+    private func printLabelWidth() {
+        print("SystemFontLabel.width: \(systemFontLabel.frame.width) pt, with font: \(systemFontLabel.font!)")
+        print("CustomFontLabel.width: \(customFontLabel.frame.width) pt, with font: \(customFontLabel.font!)")
+    }
 }
 
