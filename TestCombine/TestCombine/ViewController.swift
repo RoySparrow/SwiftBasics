f3ca9102dc485f0566a31abf1a2ab7fab471b4fa
RoyLi <royli@hahow.in>
2022-02-22 15:54:01 +0800

    add: SnapKit and test value binding

--- TestCombine/TestCombine/ViewController.swift
+++ TestCombine/TestCombine/ViewController.swift
@@ -5,21 +5,94 @@
 //  Created by RoyLi on 2022/2/22.
 //
 
+import Combine
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
 
     private lazy var viewModel = ViewModel()
     
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .black
+        label.textColor = .white
+        return label
+    }()
+    
+    private lazy var increaseBtn: UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .red
+        btn.setTitle("＋", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.addTarget(self, action: #selector(onIncreaseBtnTapped(sender:)), for: .touchUpInside)
+        return btn
+    }()
+    
+    private lazy var decreaseBtn: UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .red
+        btn.setTitle("−", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.addTarget(self, action: #selector(onDecreaseBtnTapped(sender:)), for: .touchUpInside)
+        return btn
+    }()
+    
+    private var bags = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupConstraints()
+        setupBinding()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        viewModel.callAPI()
+//        viewModel.callAPI()
+//        viewModel.testMap()
+    }
+    
+    private func setupConstraints() {
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+
+        view.addSubview(increaseBtn)
+        increaseBtn.snp.makeConstraints { make in
+            make.width.equalTo(150.0)
+            make.height.equalTo(75.0)
+            make.bottom.equalTo(label.snp.top).offset(-20.0)
+            make.centerX.equalToSuperview()
+        }
+
+        view.addSubview(decreaseBtn)
+        decreaseBtn.snp.makeConstraints { make in
+            make.width.equalTo(150.0)
+            make.height.equalTo(75.0)
+            make.top.equalTo(label.snp.bottom).offset(20.0)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    private func setupBinding() {
+        viewModel.$value
+            .sink { [weak self] value in
+                self?.label.text = "\(value)"
+            }
+            .store(in: &bags)
+    }
+    
+    @objc
+    private func onIncreaseBtnTapped(sender: UIButton) {
+        viewModel.increase()
+    }
+    
+    @objc
+    private func onDecreaseBtnTapped(sender: UIButton) {
+        viewModel.decrease()
     }
 }
 
--- TestCombine/TestCombine/ViewModel.swift
+++ TestCombine/TestCombine/ViewModel.swift
@@ -5,12 +5,65 @@
 //  Created by RoyLi on 2022/2/22.
 //
 
+import Combine
 import Foundation
 
 class ViewModel {
+    
+    private let a = CurrentValueSubject<Int, Error>(10)
+    
+    private var bags: [AnyCancellable] = []
+    
+    @Published
+    private(set) var value = 0
     
     func callAPI() {
 //        NetworkHelper.shared.call()
         NetworkHelper.shared.callWithCombine()
     }
+    
+    func testMap() {
+//        let value = 10
+//        value.words.publisher
+//            .map({ $0 + 1 })
+//            .sink(receiveValue: { value in
+//                print(value)
+//            }).store(in: &bags)
+        
+//        [1, 2, 3, 4, 5]
+//            .publisher
+//            .filter({ $0 >= 3 })
+//            .sink { value in
+//                print(value)
+//            }.store(in: &bags)
+        
+//        a.sink { completion in
+//            print(completion)
+//        } receiveValue: { value in
+//            print(value)
+//        }.store(in: &bags)
+//        a.value += 1
+//        a.value = 60
+//        a.value = -100
+    }
+    
+    func increase() {
+        value += 1
+    }
+    
+    func decrease() {
+        value -= 1
+    }
+    
+    deinit {
+        bags.forEach({ $0.cancel() })
+        bags.removeAll()
+    }
 }
+
+//struct Binding {
+//
+//    let onIncreaseBtnTapped: AnyPublisher<Void, Error>
+//
+//    let onDecreaseBtnTapped: AnyPublisher<Void, Error>
+//}
